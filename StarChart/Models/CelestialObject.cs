ab70ad9aca27ef5d2032d7acc62669ef3e8d33d5
Zaneta Matuszewska <Zaneta.Matuszewska@s3connectedhealth.com>
2021-12-16 12:20:00 +0100

    First class

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -15,5 +16,7 @@ namespace StarChart.Models
         
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
+
+        public TimeSpan OrbitalPeriod { get; set; }
     }
 }
